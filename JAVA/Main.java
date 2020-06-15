b5f5c147cf65c2a89e0f5fb425be9871ff6ac27b
yttr309 <63216289+yttr309@users.noreply.github.com>
2020-06-15 09:19:09 +0900

    문제2

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,11 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    int sum=0;
-    for(int i=1;i<=10;i++){
-      sum = sum + i;
-      System.out.println(i);
+    for(int i=0;i<=5;i++){
+      for(int j=0;j<=i;j++){
+        System.out.print('*');
+      }
+      System.out.println();
     }
-    System.out.println(sum);
-
+    for(int i=1;i<=5;i++){
+      for(int j=5;j>=i;j--){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+    
   }
 }
